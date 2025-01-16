c9635cb7a3954237b7c471b406acde1b67323d9c
asm-hash <officialasmohammed06@gmail.com>
2025-01-15 20:25:14 -0600

    code puhsed first-time

---
+++ src/main/java/com/ahsan/Srping_Jenkins_Phase1/SrpingJenkinsPhase1Application.java
@@ -0,0 +1,24 @@
+package com.ahsan.Srping_Jenkins_Phase1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SrpingJenkinsPhase1Application {
+
+
+	@GetMapping("/greetings/{name}")
+	public String greetings(@PathVariable String name)
+	{
+		return "Hello " + name + " This is the Demo of using Jenkins for CI/CD with Configuration-Approach.";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingJenkinsPhase1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahsan/Srping_Jenkins_Phase1/SrpingJenkinsPhase1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahsan.Srping_Jenkins_Phase1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingJenkinsPhase1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
